27941c0d9e35e825a1e485d31d89da1b3bb6862c
Casper Overholm Elkrog <support.open-source@elkrog.me>
2023-04-01 17:23:01 +0200

    Minor refactoring for readability

--- Gateway/Components/Routing/Maps/YarpRouteConfigMaps.cs
+++ Gateway/Components/Routing/Maps/YarpRouteConfigMaps.cs
@@ -11,31 +11,30 @@ public class YarpRouteConfigMaps : Profile
             .ForMember(x => x.RouteId, opt => opt.MapFrom(o => o.Id))
             .ForMember(x => x.ClusterId, opt => opt.MapFrom((_, _, _, context) => context.Items["ClusterId"]))
             .ForMember(d => d.AuthorizationPolicy, opt => opt.MapFrom((_, _, _, context) => context.Items["AuthorizationPolicy"]))
-            .ForMember(x => x.Match, opt => opt.MapFrom((s, _, _, context) =>
-            {
-                return new global::Yarp.ReverseProxy.Configuration.RouteMatch
-                {
-                    Methods = s.Methods,
-                    Hosts = s.Hosts,
-                    Headers =
-                        s.Headers?.Select(context.Mapper
-                                .Map<HeaderMatch, global::Yarp.ReverseProxy.Configuration.RouteHeader>).ToList()
-                            .AsReadOnly() ??
-                        Array.Empty<global::Yarp.ReverseProxy.Configuration.RouteHeader>().AsReadOnly(),
-                    Path = s.Path,
-                    QueryParameters = s.QueryParameters?.Select(
-                                              context.Mapper
-                                                  .Map<QueryParameterMatch,
-                                                      global::Yarp.ReverseProxy.Configuration.RouteQueryParameter>)
-                                          .ToList().AsReadOnly() ??
-                                      Array.Empty<global::Yarp.ReverseProxy.Configuration.RouteQueryParameter>()
-                                          .AsReadOnly()
-                };
-            }))
+            .ForMember(x => x.Match, opt => opt.MapFrom((route, _, _, context) => GetRouteMatch(route, context)))
             .ReverseMap();
-        CreateMap<HeaderMatch, global::Yarp.ReverseProxy.Configuration.RouteHeader>()
+        CreateMap<HeaderMatch, Yarp.ReverseProxy.Configuration.RouteHeader>()
             .ReverseMap();
-        CreateMap<QueryParameterMatch, global::Yarp.ReverseProxy.Configuration.RouteQueryParameter>()
+        CreateMap<QueryParameterMatch, Yarp.ReverseProxy.Configuration.RouteQueryParameter>()
             .ReverseMap();
+    }
+    
+    private static Yarp.ReverseProxy.Configuration.RouteMatch GetRouteMatch(RouteConfig route, ResolutionContext context)
+    {
+        return new Yarp.ReverseProxy.Configuration.RouteMatch
+        {
+            Methods = route.Methods,
+            Hosts = route.Hosts,
+            Headers = route.Headers
+                          ?.Select(context.Mapper.Map<HeaderMatch, Yarp.ReverseProxy.Configuration.RouteHeader>)
+                          .ToList().AsReadOnly() ??
+                      Array.Empty<Yarp.ReverseProxy.Configuration.RouteHeader>().AsReadOnly(),
+            Path = route.Path,
+            QueryParameters = route.QueryParameters
+                                  ?.Select(context.Mapper
+                                      .Map<QueryParameterMatch, Yarp.ReverseProxy.Configuration.RouteQueryParameter>)
+                                  .ToList().AsReadOnly() ??
+                              Array.Empty<Yarp.ReverseProxy.Configuration.RouteQueryParameter>().AsReadOnly()
+        };
     }
 }
